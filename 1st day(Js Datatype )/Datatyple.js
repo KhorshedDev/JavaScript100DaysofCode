3008a808a7ba71ca1dc1fe07eb4c54582cdc90cf
Khorshed alam <khorshedblog@yahoo.com>
2019-12-13 15:44:56 +0600

    1st day added

---
+++ 1st day(Js Datatype )/Datatyple.js
@@ -0,0 +1,71 @@
+//JavaScript has two type of comment 
+//Frist type( // ) already we are using ,it is only use for inline comment.
+/*
+  And  this is a Example of secound type(/*) of comment.It is use for multiline comment.
+*/
+
+//JavaScript variable decleard  by using let,var or const;
+
+//JavaScript variable name must  start with a letter,You cann't use number as a first number,you cann't use reserve word as valiable name like for,while,let etc.
+
+let example;//it can be update ;
+
+var example2;//it has some hoisting problem
+
+const example3;//it can't be update or reasign;
+
+//JavaScript has 5 Data type 
+
+//Number
+ let num1 = 5;//"=" used for asign a data ;
+
+ var nam2 = 20 //number can be integar or float ,it doesn't metter in JavaCript;
+
+ const num3 = 24.67;
+
+//String
+ 
+ let name = "Khorshed" //you must use quate('') for declaring string variable ,it can be single quate or doble("");
+
+//Bolean
+
+let isStart = true //notice? here we start with small letter then used capital letter that called camelCase most used format in JavaScript;
+
+//null
+
+let score = null //null means NaN ;
+
+//Undifined
+
+let lastName = undefined; //undefined use when you want a variable to declare  without it's value;
+
+
+
+//You can do basic math to asign a variable;
+
+let sum = 20+25;
+
+let squer = 10*10;
+
+let divition= 10/5;
+
+
+//you can asign to string together;
+
+let myName="Khorshed" + "alam";
+
+//you can first decleare a varible and than asign it;
+
+let age;
+
+age = 20;
+
+
+//you can use variable to declare a variable;
+
+let firstName = "Khorshed";
+let lastName  = "alam";
+
+let fullName= firstName +" "+ lastName;//space isn't count in JavaScript ,You have to put it where you want.
+
+
---
+++ 1st day(Js Datatype )/Hello-world.js
@@ -0,0 +1,8 @@
+//alart() used for browser alert;
+
+
+alert('Hello World');
+
+//console.log() used for printing massage for developer console;
+
+console.log('Hello World');
