09094d4ab47718fd6168d6c2a202b1676d24e551
Khorshed alam <khorshedblog@yahoo.com>
2019-12-16 22:29:25 +0600

    4th day addad

---
+++ 4th day(loop)/loop.js
@@ -0,0 +1,33 @@
+//for loop 
+ 
+ for(let i =0 ; i< 5;i++){
+ 	console.log('for loop output')
+ }
+
+ //while loop 
+let n=1
+
+ while(n<7 ){
+ 	console.log('while loop output')
+ 	n++;
+ }
+ // do while loop
+ let d = 1;
+ do{
+ 	console.log('do while output')
+    d++;
+ }while(d < 1)
+
+ //break and continue
+
+ let b = 1;
+
+ while(b < 7){
+ 	console.log('break')
+ 
+ 	if(b===3){
+ 		break;
+ 	}
+
+ 	b++;
+ }
